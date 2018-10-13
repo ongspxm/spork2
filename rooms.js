283eba5d84013369f71add1cf19c1dc13fe31c14
ongspxm <ongspxm@gmail.com>
2018-10-14 00:33:30 +0800

    add framework for rooms

---
+++ rooms.js
@@ -0,0 +1,22 @@
+const dbase = require('dbase');
+
+const DB_NAME = 'rooms';
+
+function getRooms(from=0){
+}
+
+function newRoom(rm){
+  // ({user, name, vacancy}) => True
+}
+
+function updateRoom(obj){
+  // ({user, name, vacancy}) => True
+}
+
+function newImg(obj){
+  // ({r_id, picData}) => {imgur, link, r_id, dhash}
+}
+
+function delImg(obj){
+  // ({r_id, imgurId}) => True
+}
